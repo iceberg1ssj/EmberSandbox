ad47304d1146ef2553f3ce7990d017e6616bf91d
vijayee.kulkaa@hushmail.com <vijayee.kulkaa@hushmail.com>
2014-05-28 16:31:43 -0400

    Update

--- source/javascripts/app.coffee
+++ source/javascripts/app.coffee
@@ -14,6 +14,7 @@ require
     topojson: '../vendor/topojson/topojson'
     setupRotatingGlobe:'../vendor/planetary.js/script/setup.rotating.globe'
     knob:'../vendor/jquery-knob/js/jquery.knob'
+    peerjs: '../vendor/peerjs/peer'
   ['jquery','handlebars', 'ember','d3', 'knob']
   ->
     App = Ember.Application.create()
@@ -29,7 +30,38 @@ require
 
 
       setupGlobe:->
-        $(".dial").knob()
+        today = new Date();
+        today_abs = new Date()
+        today_abs.setHours(0)
+        today_abs.setMinutes(0)
+        today_abs.setSeconds(0)
+        today_secs = (today.getTime() - today_abs.getTime()) / 1000
+        today_remaining=86500 - today_secs
+
+        $(".dial").knob {
+          min:0
+          max:86500
+          fgColor: '#e59f63'
+          change: ->
+            today = new Date();
+            today_abs = new Date()
+            today_abs.setHours(0)
+            today_abs.setMinutes(0)
+            today_abs.setSeconds(0)
+            today_secs = (today.getTime() - today_abs.getTime()) / 1000
+            today_remaining=86500 - today_secs
+            if $(".dial").val() >= today_remaining
+              $(".dial").val(today_remaining)
+            console.log($(".dial").val() >= today_remaining)
+        }
+        client= new peer({host:'localhost', port: 9000})
+        client.on 'open',(id) ->
+          console.log('My peer ID is: ' + id)
+
+        $(".dial").val(today_remaining)
+        setInterval ->
+          $(".dial").val($(".dial").val()-1).trigger('change')
+        ,1000
         require ['setupRotatingGlobe']
         ###
         setTimeout(->
--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -18,7 +18,8 @@
       d3: '../vendor/d3/d3',
       topojson: '../vendor/topojson/topojson',
       setupRotatingGlobe: '../vendor/planetary.js/script/setup.rotating.globe',
-      knob: '../vendor/jquery-knob/js/jquery.knob'
+      knob: '../vendor/jquery-knob/js/jquery.knob',
+      peerjs: '../vendor/peerjs/peer'
     }
   }, ['jquery', 'handlebars', 'ember', 'd3', 'knob'], function() {
     var App;
@@ -33,7 +34,43 @@
         return this.render();
       },
       setupGlobe: function() {
-        $(".dial").knob();
+        var client, today, today_abs, today_remaining, today_secs;
+        today = new Date();
+        today_abs = new Date();
+        today_abs.setHours(0);
+        today_abs.setMinutes(0);
+        today_abs.setSeconds(0);
+        today_secs = (today.getTime() - today_abs.getTime()) / 1000;
+        today_remaining = 86500 - today_secs;
+        $(".dial").knob({
+          min: 0,
+          max: 86500,
+          fgColor: '#e59f63',
+          change: function() {
+            today = new Date();
+            today_abs = new Date();
+            today_abs.setHours(0);
+            today_abs.setMinutes(0);
+            today_abs.setSeconds(0);
+            today_secs = (today.getTime() - today_abs.getTime()) / 1000;
+            today_remaining = 86500 - today_secs;
+            if ($(".dial").val() >= today_remaining) {
+              $(".dial").val(today_remaining);
+            }
+            return console.log($(".dial").val() >= today_remaining);
+          }
+        });
+        client = new peer({
+          host: 'localhost',
+          port: 9000
+        });
+        client.on('open', function(id) {
+          return console.log('My peer ID is: ' + id);
+        });
+        $(".dial").val(today_remaining);
+        setInterval(function() {
+          return $(".dial").val($(".dial").val() - 1).trigger('change');
+        }, 1000);
         return require(['setupRotatingGlobe']);
 
         /*
